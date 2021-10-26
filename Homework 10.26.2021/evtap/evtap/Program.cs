0d0aa151baa52a40cba5d929d535f0f361c8e167
zeynaloruccodeacademy <201806305@code.edu.az>
2021-10-26 23:12:29 +0400

    commit 10/26

---
+++ Homework 10.26.2021/evtap/evtap/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace evtap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1.1
+            //Console.WriteLine("Toplama emeliyyati");
+            //Console.WriteLine("------------------------------------");
+            //Console.Write("1ci Ededi daxil edin: ");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("2ci Ededi daxil edin: ");
+            // int b = Convert.ToInt32(Console.ReadLine());
+
+            //int c = a + b;
+
+            //Console.WriteLine("------------------------------------");
+
+            //Console.Write("Ededlerin cemi: " + c);
+         
+
+
+        }
+
+
+
+
+    }
+}
