17b528778072e596f22ac932f56857652d64fb87
zeynaloruccodeacademy <92145504+zeynaloruccodeacademy@users.noreply.github.com>
2021-10-27 08:10:03 +0400

    Add files via upload

---
+++ Homework 10.26.2021/evtap/metod 1/ConsoleApp3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1-ci ededi daxil edin");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2-ci eded: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(Test1(x,y));
+
+
+
+
+
+
+
+        }
+        static string Test1(int x, int y)
+        {
+
+            string k = ($"{x} and {y} ededlerinin toplami {x+y}, hasili {x*y}");
+            return k;
+
+
+
+        }
+    } 
+}
