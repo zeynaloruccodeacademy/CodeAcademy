389a23b7ca5c7158e2d0337a9331e249da2985b5
zeynaloruccodeacademy <92145504+zeynaloruccodeacademy@users.noreply.github.com>
2021-10-27 08:22:06 +0400

    Add files via upload

---
+++ Homework 10.26.2021/evtap/metod 2/metod 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace metod_2
+{
+    class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                while (true)
+                {
+                    Console.Write("1-ci eded : ");
+                    int x = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("2-ci eded : ");
+                    int y = Convert.ToInt32(Console.ReadLine());
+                    if (x < y)
+                    {
+                        Console.WriteLine($"{x} ve {y} araliqdaki ededlerin cemi {ara(x, y)}");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nbirinci eded ikinci ededden kicik olmalidir\n");
+                    }
+                }
+
+
+            }
+            static int ara(int x, int y)
+            {
+                int sum = 0;
+                for (; x <= y; x++)
+                {
+                    sum = sum + x;
+                }
+                return sum;
+            }
+       
+    }
+}
