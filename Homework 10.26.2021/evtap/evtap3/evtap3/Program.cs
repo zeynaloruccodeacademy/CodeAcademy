248d449a477412f4553c444fcf884d9518355f91
zeynaloruccodeacademy <92145504+zeynaloruccodeacademy@users.noreply.github.com>
2021-10-27 08:10:46 +0400

    Add files via upload

---
+++ Homework 10.26.2021/evtap/evtap3/evtap3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace evtap3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(-1 + 4 * 6);
+            Console.WriteLine((35 + 5)%7);
+            Console.WriteLine(14 + (-4 * 6 / 11));
+            Console.WriteLine(2+(15/6*1)-(7%2));
+
+        }
+    }
+}
